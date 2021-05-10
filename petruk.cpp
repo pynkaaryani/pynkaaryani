563aed17a6b27be44c6475ebb8c96572b511d047
anzaniy <83295231+anzaniy@users.noreply.github.com>
2021-05-10 13:07:49 +0700

    Update petruk.cpp

--- petruk.cpp
+++ petruk.cpp
@@ -9,3 +9,18 @@
 using namespace std;
 const int cols = 16, rows = 15;
 
+char words[rows][cols] =      { "tgbwwinterwsesn",
+                                "aaunttmmhfoodnb",
+                                "jlwcqldzmpmvdmr",
+                                "asagmquwvvbsohi",
+                                "bwplotanadtpgnc",
+                                "rewngodjcpnatnk",
+                                "eeotwosbqharrsa",
+                                "azcgeswewnaknpb",
+                                "dinnerqodlwdcar",
+                                "onopkwmparktzcc",
+                                "qbfrogmamwpweey",
+                                "lqzqnnmrzjjsclg",
+                                "mosgzczetdbooto",
+                                "pdcrzmsngrdnrpz",
+                                "ohnkzwaterjgtra"};
